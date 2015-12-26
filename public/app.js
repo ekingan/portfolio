e087781c883c7613dbb8d5097638cb29a892fdd5
ekingan <ekingan@gmail.com>
2015-12-26 07:52:28 -0800

    commit

--- public/app.js
+++ public/app.js
@@ -9,13 +9,33 @@ angular.module('myApp', ['ui.router',
 
   .config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {
     $stateProvider
+      .state('/home', {
+        url: "/home", 
+        templateUrl: 'templates/home',
+        controller: 'HomeCtrl'
+        })
+      .state('/about', {
+        url: "/about", 
+        templateUrl: 'templates/about',
+        controller: 'MainCtrl'
+        })
+      .state('/resume', {
+        url: "/resume", 
+        templateUrl: 'templates/resume',
+        controller: 'MainCtrl'
+        })
+       .state('/contact', {
+        url: "/contact", 
+        templateUrl: 'templates/contact',
+        controller: 'MainCtrl'
+        })
       .state('posts', {
-        url: "/",
+        url: "/posts",
         templateUrl: 'templates/posts-index',
         controller: 'PostsIndexCtrl'
       });
 
-    $urlRouterProvider.otherwise("/state1");
+    $urlRouterProvider.otherwise("/home");
 
     $locationProvider.html5Mode({
         enabled: true,
